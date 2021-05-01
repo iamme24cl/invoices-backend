571b56e8f34d5145e01df50b58f96d6615543f63
Chandra Lama <iamme24cl@gmail.com>
2021-05-01 14:38:40 -0700

    added namespaced routes for Invoices

--- app/controllers/api/v1/accounts_controller.rb
+++ app/controllers/api/v1/accounts_controller.rb
@@ -1,2 +1,12 @@
 class Api::V1::AccountsController < ApplicationController
+  def index
+    accounts = Account.all 
+    render json: accounts
+  end
+  
+  def show
+    account = Account.find_by(id: params[:id])
+
+    render json: account
+  end
 end
--- app/controllers/api/v1/invoices_controller.rb
+++ app/controllers/api/v1/invoices_controller.rb
@@ -1,2 +1,10 @@
 class Api::V1::InvoicesController < ApplicationController
+  def index 
+    account = Account.find_by(id: params[:account_id])
+
+    invoices = account.invoices
+
+    render json: invoices
+    
+  end
 end
