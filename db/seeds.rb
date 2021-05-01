536e7ed5595bcc6f87893e8e3ab4dfdc87a38e93
Chandra Lama <iamme24cl@gmail.com>
2021-05-01 15:33:42 -0700

    created some seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+account = Account.create(accountname: "My Account", address: "19 Union Terrace, London, E1 3EZ, United Kingdom", password: "testing")
+
+invoice_data = {
+  payment_due: "2021-08-19",
+  description: "Re-branding",
+  payment_terms: 1,
+  client_name: "Jensen Huang",
+  client_email: "jensenh@mail.com",
+  client_address: "106 Kendell Street, Sharrington, NR24 5WQ, United Kingdom"
+}
+
+invoice = account.invoices.build(invoice_data)
+
+item_data =   {
+  name: "Brand Guidelines",
+  quantity: 1,
+  price: 1800.90,
+  total: 1800.90
+}
+
+invoice.items.build(item_data)
+
+account.save
