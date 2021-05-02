97ebd4f2ecc25f27ff985a7c6b698828535e1152
Chandra Lama <iamme24cl@gmail.com>
2021-05-01 18:10:33 -0700

    implemented invoice controller

--- app/controllers/api/v1/invoices_controller.rb
+++ app/controllers/api/v1/invoices_controller.rb
@@ -1,10 +1,47 @@
 class Api::V1::InvoicesController < ApplicationController
+  before_action :find_invoice, only: [:show, :update, :destroy]
+  before_action :find_account, only: [:index, :create, :update, :destroy]
+  
   def index 
-    account = Account.find_by(id: params[:account_id])
-
-    invoices = account.invoices
-
+    if @account
+      invoices = @account.invoices
+    else
+      invoices = Invoice.all 
+    end    
     render json: invoices
-    
   end
+
+  def show
+    render json: @invoice
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def invoice_params
+    params.require(:invoice).permit(
+      :payment_due, 
+      :description, 
+      :payment_terms, 
+      :client_name,
+      :client_email,
+      :client_address 
+    )
+  end
+
+  def find_invoice
+    @invoice = Invoice.find_by(id: params[:id])
+  end
+
+  def find_account
+    @account = Account.find_by(id: params[:account_id])
+  end  
 end
