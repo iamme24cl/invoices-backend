8374efa2842a44b76cd79c8cade7002646e9edba
Chandra Lama <iamme24cl@gmail.com>
2021-05-15 19:08:41 -0700

    changed Invoice serializer to render account instead of account_id

--- app/serializers/invoice_serializer.rb
+++ app/serializers/invoice_serializer.rb
@@ -1,4 +1,4 @@
 class InvoiceSerializer < ActiveModel::Serializer
-  attributes :id, :random_code, :payment_due, :description, :payment_terms, :status, :client_name, :client_email, :client_address, :invoice_total, :items, :account_id
+  attributes :id, :random_code, :payment_due, :description, :payment_terms, :status, :client_name, :client_email, :client_address, :invoice_total, :items, :account
   
 end
