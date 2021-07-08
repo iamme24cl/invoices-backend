4d5715f0ed9ea14afa9d03fcd759d209222b51d6
Chandra Lama <iamme24cl@gmail.com>
2021-07-08 13:28:06 -0700

    init

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  # root 'api/v1/accounts#index'
+
   namespace :api do
     namespace :v1 do
       resources :accounts, only: [:index, :show, :create, :update] do
