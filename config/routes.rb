981e3f07c8054ddde507f76cbb474757e00aa12c
chandra <iamme24cl@gmail.com>
2021-12-24 11:04:34 -0800

    removed routes for items and added a root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,13 @@
 Rails.application.routes.draw do
-
-  # root 'api/v1/accounts#index'
+  root 'api/v1/api_keys#index'
 
   namespace :api do
     namespace :v1 do
       resources :api_keys, path: 'api-keys', only: %i[index create destroy] 
-      
       resources :accounts, only: [:index, :show, :create, :update] do
-        resources :invoices, only: [:index, :create, :update, :destroy] do
-          resources :items, only: [:update, :destroy]
-        end
+        resources :invoices, only: [:index, :create, :update, :destroy] 
       end
     end
   end
-
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
