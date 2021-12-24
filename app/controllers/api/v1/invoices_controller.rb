83d999517fb1dd77e2f9596c0b8ad274940d4cb3
chandra <iamme24cl@gmail.com>
2021-12-24 11:08:49 -0800

    deleted all actions in Items controller and enabled Items crud via Invoice, changed Invoice index to render in descending order of creation

--- app/controllers/api/v1/invoices_controller.rb
+++ app/controllers/api/v1/invoices_controller.rb
@@ -4,9 +4,9 @@ class Api::V1::InvoicesController < ApplicationController
   
   def index 
     if @account
-      invoices = @account.invoices
+      invoices = @account.invoices.order created_at: :desc
     else
-      invoices = Invoice.all 
+      invoices = Invoice.all.order created_at: :desc 
     end    
     render json: invoices
   end
@@ -18,25 +18,27 @@ class Api::V1::InvoicesController < ApplicationController
   def create
     invoice = @account.invoices.build(invoice_params)
     if invoice.set_random_code && invoice.save
-      render json: invoice.account, status: :accepted
+      render json: invoice, status: :accepted
     else
       render json: { errors: invoice.errors.full_messages }
     end
   end
 
   def update
+    @invoice.items.destroy_all
     if @invoice.update(invoice_params)
       @invoice.update_total
-      render json: @invoice.account, status: :accepted
+      render json: @invoice, status: :accepted
     else
       render json: { errors: @invoice.errors.full_messages }
     end
   end
 
   def destroy
+    id = @invoice.id
     @invoice.destroy
 
-    render json: @account, status: :accepted
+    render json: { message: "Successfully deleted invoice with id: #{id}"}, status: 200
   end
 
   private
