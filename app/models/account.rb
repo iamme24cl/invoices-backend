5d2b60e20ab588a035fa7acae325f75151e41722
chandra <iamme24cl@gmail.com>
2022-01-01 09:11:51 -0800

    .

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,7 +1,6 @@
 class Account < ApplicationRecord
-  has_many :api_keys, as: :bearer, dependent: :delete_all
-
-  has_many :invoices
   validates :accountname, :email, :address, presence: true
   has_secure_password
+  has_many :api_keys, as: :bearer, dependent: :delete_all
+  has_many :invoices
 end
